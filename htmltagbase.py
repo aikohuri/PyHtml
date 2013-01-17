14a27b44f13634ca2a1c780e6b924a0a1bb3e8f9
Akihiro Minoguchi <makihiro.06.06@gmail.com>
2013-01-17 04:36:22 -0600

    modified to replace '<' and '>' with html entity

--- htmltagbase.py
+++ htmltagbase.py
@@ -2,6 +2,50 @@ from collections import deque
 from os.path import abspath, expanduser
 
 
+SP    = '&nbsp;'  # non-breaking space
+LT    = '&lt;'    # less than
+GT    = '&gt;'    # greater than
+AMP   = '&amp;'   # ampersand
+CENT  = '&cent;'  # cent
+POUND = '&pound;' # pound
+YEN   = '&yen;'   # yen
+EURO  = '&euro;'  # euro
+SECT  = '&sect;'  # section
+COPY  = '&copy;'  # copyright
+REG   = '&reg;'   # registered trademark
+TRADE = '&trade;' # trademark
+
+# This DTD contains all HTML elements and attributes, but does NOT INCLUDE
+# presentational or deprecated elements (like font). Framesets are not allowed.
+HTML4_01_STRICT = 'HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"'
+
+# This DTD contains all HTML elements and attributes, INCLUDING presentational
+# and deprecated elements (like font). Framesets are not allowed.
+HTML4_01_TRANSITIONAL = 'HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"'
+
+# This DTD is equal to HTML 4.01 Transitional, but allows the use of frameset
+# content.
+HTML4_01_FRAMESET = 'HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd"'
+
+# This DTD contains all HTML elements and attributes, but does NOT INCLUDE
+# presentational or deprecated elements (like font). Framesets are not allowed.
+# The markup must also be written as well-formed XML.
+XHTML1_0_STRICT = 'html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"'
+
+# This DTD contains all HTML elements and attributes, INCLUDING presentational
+# and deprecated elements (like font). Framesets are not allowed. The markup 
+# must also be written as well-formed XML.
+XHTML1_0_TRANSITIONAL = 'html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"'
+
+# This DTD is equal to XHTML 1.0 Transitional, but allows the use of frameset 
+# content.
+XHTML1_0_FRAMESET = 'html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd"'
+
+# This DTD is equal to XHTML 1.0 Strict, but allows you to add modules (for 
+# example to provide ruby support for East-Asian languages).
+XHTML1_1 = 'html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"'
+
+
 class TagBase:
 
     def __init__(self, tagName, *contents, **kargs):
@@ -75,7 +119,7 @@ class TagBase:
                                attrs,
                                self.etag if self.etag is not None else ' />')
         else:
-            contents = [c if not hasattr(c,'toString') else c.toString(uppercase) for c in self.contents if c]
+            contents = [c.replace('<',LT).replace('>',GT) if not hasattr(c,'toString') else c.toString(uppercase) for c in self.contents if c]
             contents = ''.join(contents)
 
             data = [self.stag if self.stag is not None else '<%s%s>' % (tagName, attrs),
@@ -100,7 +144,7 @@ class TagBase:
             data = '%s<%s%s />' % (offset, tagName, attrs)
         else:
             newOffset = ''.join([offset, indentChar])
-            contents = '\n'.join(['%s%s' % (newOffset, c) if not hasattr(c,'toPrettyString') else c.toPrettyString(indentChar, newOffset, uppercase)
+            contents = '\n'.join(['%s%s' % (newOffset, c.replace('<',LT).replace('>',GT)) if not hasattr(c,'toPrettyString') else c.toPrettyString(indentChar, newOffset, uppercase)
                                   for c in self.contents if c])
             data = ['%s%s' % (offset, self.stag) if self.stag is not None else '%s<%s%s>' % (offset, tagName, attrs),
                     '%s' % (contents),
